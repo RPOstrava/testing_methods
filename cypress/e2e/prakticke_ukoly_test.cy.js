f9cb58bf10f29d029e32be2b738b0ed77c92545c
RPOstrava <warwarc097@gmail.com>
2024-03-06 15:25:56 +0100

    2nd test -unit test on page 2 done

--- cypress/e2e/prakticke_ukoly_test.cy.js
+++ cypress/e2e/prakticke_ukoly_test.cy.js
@@ -3,3 +3,30 @@ describe('Hlavní stránka', () => {
         cy.visit('index.html');
 
     });
+
+    it("Unit tests", () => {
+        cy.visit("prakticke-ukoly.html");
+
+        const testing_links = [
+            {"Test funkčnosti tlačítka" : "zda tlačítko provádí požadovanou akci"},
+            {"Test validace formuláře" : "správně validuje vstupy"},
+            {"Test ověření textu" : "správnost textu na stránce"},
+            {"Test načasování" : "časové aspekty aplikace"},
+            {"Test navigace": "ověření správnosti navigace"},
+            {"Test kompatibility prohlížeče" : " webová stránka nebo aplikace správně zobrazována"},
+            {"Test API" : "API funguje správně"},
+            {"Test webové služby" : "funkčnost a integritu webové služby"},
+            {"Test zabezpečení" : "je aplikace nebo stránka chráněna"},
+        ];
+        testing_links.forEach((link) => {
+            const testTitle = Object.keys(link)[0];
+            const testDescription = link[testTitle];         
+            cy.contains(testTitle).click();
+            cy.contains(testDescription).should("be.visible");
+            cy.screenshot();
+        }); 
+        
+    });
+
+});
+        
