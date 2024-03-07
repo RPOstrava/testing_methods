2276a734f1d2cff5830bb3f2eb6bfe07d178c9e2
RPOstrava <warwarc097@gmail.com>
2024-03-07 14:14:34 +0100

    repaired smoke test

--- cypress/e2e/smoke_test_index_html.cy.js
+++ cypress/e2e/smoke_test_index_html.cy.js
@@ -3,19 +3,18 @@ describe("Smoke test", () => {
         cy.visit('index.html');
     
             
-        const links = [
-        "teorie",
-        "prakticke-ukoly",
-        "diskuzni-forum",
-        "odevzdat-projekt"
+        const images = [
+            "theory.png",
+            "prakticke-ukoly.png",
+            "diskuzni-forum.png",
+            "odevzdat-projekt.png"
         ];
 
-        links.forEach((link) => {
-            cy.contains("links").click();
+        images.forEach((image) => {
+            cy.get(`img[src*="${image}"]`).click();
             cy.wait(1000);
             cy.screenshot();
             cy.go("back");
-
         });
 
     });
